59e2fbf0f9c567cdaebc2fbe20a50831d5d64b55
ruldiaz <rulhdiaz@gmail.com>
2024-08-31 23:11:59 -0600

    commit

--- frontend/profile.js
+++ frontend/profile.js
@@ -12,7 +12,27 @@ window.onload = async () => {
             credentials: 'include'
          });
 
-         debugger
+         if(response.ok){
+            const { user } = await response.json();
+
+            const userInfo = document.getElementById('user-info');
+
+            const firstName = document.createElement('p');
+            firstName.innerText = `First name: ${user.name.first}`;
+
+            const lastName = document.createElement('p');
+            lastName.innerText = `Last name: ${user.name.last}`;
+
+            const email = document.createElement('p');
+            email.innerText = `Email: ${user.email}`;
+
+            userInfo.appendChild(firstName);
+            userInfo.appendChild(lastName);
+            userInfo.appendChild(email);
+         }else{
+            const { msg } = await response.json();
+            throw new Error(msg);
+         }
       } catch (error) {
          alert(`Failed to load profile ${error?.message || 'Uknown error'}`);
          window.location.href = 'index.html';
