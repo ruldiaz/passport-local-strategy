f404be7f0fe248c0b5221c246e57c1bf86cbcad5
ruldiaz <rulhdiaz@gmail.com>
2024-08-31 22:10:23 -0600

    commit

--- frontend/index.js
+++ frontend/index.js
@@ -12,11 +12,33 @@ window.onload = ()=>{
 document.getElementById('login-button').addEventListener('click', async (event)=>{
    event.preventDefault();
    const email = document.getElementById('email').value;
-   const password = document.getElementById('password');
+   const password = document.getElementById('password').value;
    const errMsgDiv = document.getElementById('error-message');
 
    if(!email || !password){
       errMsgDiv.innerHTML = 'Incomplete form. All fields required.';
       return;
+   }
+
+   try {
+      const response = await fetch('http://localhost:3000/api/login', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+            username: email,
+            password
+         }),
+         
+      });
+      if(response.ok){
+         const {msg, redirecTo} = await response.json();
+      }
+      
+      debugger
+
+   } catch (error) {
+      errMsgDiv.innerHTML = error?.message || 'Failed to login.';
    }
 })
