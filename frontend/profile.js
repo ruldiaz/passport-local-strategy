f1dbf90b2684bc3fcd140d0503d4e1225b26d249
ruldiaz <rulhdiaz@gmail.com>
2024-08-31 22:59:53 -0600

    commit

--- frontend/profile.js
+++ frontend/profile.js
@@ -1,6 +1,21 @@
-window.onload = () => {
+window.onload = async () => {
    if(!sessionStorage.getItem('isAuthenticated')){
       alert('access denied');
       window.location.href = 'index.html';
+   }else{
+      try {
+         const response = await fetch('http://localhost:3000/api/user', {
+            method: 'GET',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+         });
+
+         debugger
+      } catch (error) {
+         alert(`Failed to load profile ${error?.message || 'Uknown error'}`);
+         window.location.href = 'index.html';
+      }
    }
 }
